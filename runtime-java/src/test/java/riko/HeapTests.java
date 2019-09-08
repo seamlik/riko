4703d16fab2e6b18b9784c25fab1ec9b2dce900b
殷啟聰 | Kai-Chung Yan <seamlik@debian.org>
2019-09-08 22:12:13 +0800

    Basic Java unit test for `riko.Heap`

---
+++ runtime-java/src/test/java/riko/HeapTests.java
@@ -0,0 +1,33 @@
+package riko;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HeapTests {
+
+  static class DummyHeap extends Heap {
+
+    DummyHeap() {
+      super(0);
+    }
+
+    @Override
+    protected void drop() {
+      // Nothing
+    }
+
+    void run() {
+      assertNotFreed();
+      // Nothing
+    }
+  }
+
+  @Test
+  void throwsWhenUseAfterFree() {
+    final DummyHeap obj = new DummyHeap();
+    Assertions.assertThrows(AssertionError.class, () -> {
+      obj.close();
+      obj.run();
+    });
+  }
+}
