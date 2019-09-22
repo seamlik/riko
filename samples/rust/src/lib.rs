4c543c22ed8196f452b00f66514cd92765e519ec
殷啟聰 | Kai-Chung Yan <seamlik@debian.org>
2019-09-22 17:26:58 +0800

    Samples: Add `Result` and `Option` usage

--- samples/rust/src/lib.rs
+++ samples/rust/src/lib.rs
@@ -1,6 +1,8 @@
 mod heap;
 mod serde;
 
+use std::result::Result;
+
 #[riko::fun]
 fn nothing() {
     println!("A no-in no-out function.");
@@ -14,4 +16,13 @@ fn _i32(a: i32, b: i32) -> i32 {
 #[riko::fun(name = "rename")]
 fn rename_ffi() {
     println!("A function to be renamed.");
+}
+
+#[riko::fun(sig = "(I32, I32) -> I32")]
+fn result_option(a: Option<i32>, b: Option<i32>) -> Result<Option<i32>, std::fmt::Error> {
+    match (a, b) {
+        (Some(a_value), Some(b_value)) => Ok(Some(a_value + b_value)),
+        (None, None) => Err(std::fmt::Error),
+        _ => Ok(None),
+    }
 }
